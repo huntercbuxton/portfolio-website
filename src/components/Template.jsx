22b8acc630227bc45802036365b67f24d3a43ca2
Hunter Buxton <hunter.buxton@protonmail.com>
2026-03-24 04:53:47 -0600

    remove unused files + code formatting

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,12 +1,12 @@
-
-import * as StringTemplates from '../util/StringTemplates.js';
 import './template.css'
+ 
+export const copyright = (year, holder) => `${String.fromCharCode(169)} ${year} ${holder}`;
 
 export const Footer = (props) => {
-    const cpy = StringTemplates.copyright(2023, 'Hunter Buxton')
+    const cpy = copyright(2023, 'Hunter Buxton')
     return (
         <footer className="page-footer">
-              {props.children}
+            {props.children}
             <div className="stack-right">
                 <p className="footnote text-box">{cpy}</p>
             </div>
--- src/util/StringTemplates.js
+++ src/util/StringTemplates.js
@@ -1,5 +1,1 @@
 
-
-export const copyright = (year, holder) => `${String.fromCharCode(169)} ${year} ${holder}`;
-
-
